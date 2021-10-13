16a37147eedd3e06557fa6be2d6d27f9b969c373
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2021-10-13 23:01:10 +0300

    Modifying fragment for training description

--- app/src/main/java/com/trainingtimer/Training.kt
+++ app/src/main/java/com/trainingtimer/Training.kt
@@ -3,6 +3,6 @@ package com.trainingtimer
 import java.util.*
 
 data class Training(val trainingId: UUID = UUID.randomUUID(),
-                    val title: String = "",
-                    val sets: Int,
-                    val times: Int)
+                    var title: String = "",
+                    var sets: Int,
+                    var times: Int)
--- app/src/main/java/com/trainingtimer/TrainingFragment.kt
+++ app/src/main/java/com/trainingtimer/TrainingFragment.kt
@@ -1,9 +1,12 @@
 package com.trainingtimer
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import java.util.*
@@ -12,6 +15,9 @@ class TrainingFragment : Fragment() {
 
     private lateinit var training: Training
     private lateinit var titleField: EditText
+    private lateinit var setsField: EditText
+    private lateinit var timesField: EditText
+    private lateinit var restButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +33,88 @@ class TrainingFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_training, container, false)
         titleField = view.findViewById(R.id.training_title) as EditText
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                //
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                training.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //
+            }
+        }
+
+        val setsWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                //
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                training.sets = sequence.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //
+            }
+
+        }
+
+        val timesWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                //
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                training.times = sequence.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //
+            }
+
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
+        setsField.addTextChangedListener(setsWatcher)
+        timesField.addTextChangedListener(timesWatcher)
+        restButton = view?.findViewById(R.id.training_rest) as Button
+
+        restButton.apply {
+            text = training.rest.toString()
+            isEnabled = false
+        }
     }
 }
