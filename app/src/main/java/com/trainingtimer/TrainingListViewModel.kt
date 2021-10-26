f1cbec8bf84b04289365a33fd06cf3e40f90cf58
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2021-10-26 23:28:09 +0300

    Gradle deprecated functions problem

--- app/src/main/java/com/trainingtimer/TrainingListViewModel.kt
+++ app/src/main/java/com/trainingtimer/TrainingListViewModel.kt
@@ -4,16 +4,6 @@ import androidx.lifecycle.ViewModel
 
 class TrainingListViewModel : ViewModel() {
 
-    /*val trainings = mutableListOf<Training>()
-
-    init {
-        for (i in 0 until 100) {
-            val training = Training(UUID.randomUUID(), "", 0, 0)
-            training.title = "Training #$i"
-            trainings += training
-        }
-    }*/
-
     private val trainingRepository = TrainingRepository.get()
     val trainingListLiveData = trainingRepository.getTrainings()
 }
