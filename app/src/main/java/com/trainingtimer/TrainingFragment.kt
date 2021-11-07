7cea08b1cdf912baa7ae888d78a85fc582a78e12
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2021-11-07 22:50:52 +0300

    Timer back added, app keeps stopping

--- app/src/main/java/com/trainingtimer/TrainingFragment.kt
+++ app/src/main/java/com/trainingtimer/TrainingFragment.kt
@@ -15,6 +15,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.trainingtimer.databinding.FragmentTrainingBinding
 import java.time.Clock
 import java.util.*
 
@@ -31,6 +32,8 @@ class TrainingFragment : Fragment() {
     private var timerLengthSeconds = 0L
     private var timerState = TimerState.Stopped
     private var secondsRemaining = 0L
+    private var _binding: FragmentTrainingBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var training: Training
     private lateinit var titleField: EditText
@@ -44,11 +47,6 @@ class TrainingFragment : Fragment() {
         val trainingId: UUID = arguments?.getSerializable(ARG_TRAINING_ID) as UUID
         //Log.d(TAG, "args bundle training ID: $trainingId")
         trainingDetailViewModel.loadTraining(trainingId)
-
-        /*training_done.setOnClickListener { view ->
-            startTimer()
-            timerState = TimerState.Running
-        }*/
     }
 
     override fun onCreateView(
@@ -58,11 +56,16 @@ class TrainingFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_training, container, false)
         titleField = view.findViewById(R.id.training_title) as EditText
-        return view
+        //return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.trainingDone.setOnClickListener { view ->
+            startTimer()
+            timerState = TimerState.Running
+        }
         trainingDetailViewModel.trainingLiveData.observe(
             viewLifecycleOwner,
             Observer { training ->
@@ -110,6 +113,34 @@ class TrainingFragment : Fragment() {
 
     private fun updateUI() {
         titleField.setText(training.title)
+    }
+
+    private fun onTimerFinished(){
+        timerState = TimerState.Stopped
+
+        //set the length of the timer to be the one set in SettingsActivity
+        //if the length was changed when the timer was running
+        /*setNewTimerLength()
+
+        progress_countdown.progress = 0
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this)
+        secondsRemaining = timerLengthSeconds
+
+        updateButtons()
+        updateCountdownUI()*/
+    }
+
+    private fun startTimer(){
+        timerState = TimerState.Running
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onFinish() = onTimerFinished()
+
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000
+            }
+        }.start()
     }
 
     companion object{
