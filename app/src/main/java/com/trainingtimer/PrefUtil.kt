48c4d8e33bcc42d9b50a9c3b232abb5812a32894
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2021-11-18 23:26:46 +0300

    Preferences with Context, app keeps stopping

--- app/src/main/java/com/trainingtimer/PrefUtil.kt
+++ app/src/main/java/com/trainingtimer/PrefUtil.kt
@@ -3,6 +3,7 @@ package com.trainingtimer
 import android.content.Context
 import android.preference.PreferenceManager
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.preference.PreferenceFragmentCompat
@@ -19,16 +20,17 @@ class PrefUtil {
         private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID =
             "com.timer.previous_timer_length_seconds"
 
-        fun getPreviousTimerLengthSeconds(context: TrainingFragment): Long {
-            val preferences = PreferenceFragmentCompat.OnPreferenceStartFragmentCallback(TrainingFragment)
+        fun getPreviousTimerLengthSeconds(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+                //PreferenceFragmentCompat.OnPreferenceStartFragmentCallback(TrainingFragment)
                 //PreferenceFragmentCompat(context)
                 //FragmentManager.findFragment<TrainingFragment>(View(context: Context))
                 //FragmentActivity.getSupportFragmentManager
-                //PreferenceManager.getDefaultSharedPreferences(context)
+                //
             return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, 0)
         }
 
-        fun setPreviousTimerLengthSeconds(seconds: Long, context: TrainingFragment) {
+        fun setPreviousTimerLengthSeconds(seconds: Long, context: Context) {
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
             editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
             editor.apply()
@@ -36,13 +38,13 @@ class PrefUtil {
 
         private const val TIMER_STATE_ID = "com.timer.timer_state"
 
-        fun getTimerState(context: TrainingFragment): TrainingFragment.TimerState {
+        fun getTimerState(context: Context): TrainingFragment.TimerState {
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
             val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
             return TrainingFragment.TimerState.values()[ordinal]
         }
 
-        fun setTimerState(state: TrainingFragment.TimerState, context: TrainingFragment) {
+        fun setTimerState(state: TrainingFragment.TimerState, context: Context) {
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
             val ordinal = state.ordinal
             editor.putInt(TIMER_STATE_ID, ordinal)
@@ -51,12 +53,12 @@ class PrefUtil {
 
         private const val SECONDS_REMAINING_ID = "com.timer.seconds_remaining"
 
-        fun getSecondsRemaining(context: TrainingFragment): Long {
+        fun getSecondsRemaining(context: Context): Long {
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
             return preferences.getLong(SECONDS_REMAINING_ID, 0)
         }
 
-        fun setSecondsRemaining(seconds: Long, context: TrainingFragment) {
+        fun setSecondsRemaining(seconds: Long, context: Context) {
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
             editor.putLong(SECONDS_REMAINING_ID, seconds)
             editor.apply()
