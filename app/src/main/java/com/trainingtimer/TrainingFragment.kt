c783ef9665f9ac57c09afed52740808c81b15458
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2021-12-07 23:17:15 +0300

    PrefUtil with single parameter failed, app keeps stopping

--- app/src/main/java/com/trainingtimer/TrainingFragment.kt
+++ app/src/main/java/com/trainingtimer/TrainingFragment.kt
@@ -61,7 +61,7 @@ class TrainingFragment : Fragment() {
             TimerState.Stopped -> TODO()
         }
 
-        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this)
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds)
         PrefUtil.setSecondsRemaining(secondsRemaining, this)
         PrefUtil.setTimerState(timerState, this)
     }
