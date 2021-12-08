18eccc5c7896d8f44993306046380c06c7792a8e
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2021-12-08 23:39:13 +0300

    Context integrate to PrefUtil, app keeps stopping

--- app/src/main/java/com/trainingtimer/TrainingFragment.kt
+++ app/src/main/java/com/trainingtimer/TrainingFragment.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.trainingtimer.databinding.FragmentTrainingBinding
 import java.util.*
@@ -61,7 +60,7 @@ class TrainingFragment : Fragment() {
             TimerState.Stopped -> TODO()
         }
 
-        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds)
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, Context..)
         PrefUtil.setSecondsRemaining(secondsRemaining, this)
         PrefUtil.setTimerState(timerState, this)
     }
