29219166439875694be67310d308ab9226bbf224
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2023-06-12 22:28:01 +0300

    Picker fixed with old button "save"

--- app/src/main/java/com/trainingtimer/ui/TimePickerFragment.kt
+++ app/src/main/java/com/trainingtimer/ui/TimePickerFragment.kt
@@ -19,38 +19,18 @@ class TimePickerFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = TimePickerDialogBinding.inflate(LayoutInflater.from(context))
-        Log.d("TimePickerFragment", "onCreateDialog ${binding.timePicker.getCurrentMinutes()}")
-        val listener = DialogInterface.OnClickListener { _, _ ->
-            val time = (binding.timePicker.getCurrentMinutes() * 60 + binding.timePicker.getCurrentSeconds()).toLong()
-            Log.d("TimePickerFragment", "listener ${binding.timePicker.getCurrentMinutes()}")
-            Toast.makeText(context, "$time", Toast.LENGTH_SHORT).show()
-            parentFragmentManager.setFragmentResult("key", bundleOf("time" to time))
-            dismiss()
-        }
-        return AlertDialog.Builder(requireContext())
-            .setTitle("Задайте время")
-            .setView(R.layout.time_picker_dialog)
-            .setPositiveButton("save", listener)
-            .create()
-        /*binding = TimePickerDialogBinding.bind(requireView())
         return AlertDialog.Builder(requireContext())
             .setView(binding.root)
-            .create()*/
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d("TimePickerFragment", "onStart ${binding.timePicker.getCurrentMinutes()}")
+            .create()
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("TimePickerFragment", "onResume ${binding.timePicker.getCurrentMinutes()}")
-//        DialogInterface.BUTTON_POSITIVE
-//        binding .save.setOnClickListener {
-//            val time = (binding.timePicker.getCurrentMinutes() * 60 + binding.timePicker.getCurrentSeconds()).toLong()
-//            setFragmentResult("key", bundleOf("time" to time))
-//            dismiss()
-//        }
+        binding.save.setOnClickListener {
+            val time = (binding.timePicker.getCurrentMinutes() * 60
+                    + binding.timePicker.getCurrentSeconds()).toLong()
+            setFragmentResult("key", bundleOf("time" to time))
+            dismiss()
+        }
     }
 }
