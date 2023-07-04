a7e57203ac15b712fdc65ad28e67e80688523213
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2023-07-04 23:58:28 +0300

    Dao problem: id error

--- app/src/main/java/com/trainingtimer/data/TrainingDao.kt
+++ app/src/main/java/com/trainingtimer/data/TrainingDao.kt
@@ -10,19 +10,21 @@ import com.trainingtimer.domain.Training
 @Dao
 interface TrainingDao {
     //There is a problem with the query: [SQLITE_ERROR] SQL error or missing database (no such table: training)
+    //The columns returned by the query does not have the fields [id] in com.trainingtimer.domain.Training even though they are annotated as non-null or primitive. Columns returned by the query: [sets,title,times,rest,trainingId]
+    //public abstract androidx.lifecycle.LiveData<java.util.List<com.trainingtimer.domain.Training>> getTrainings();
 
     @Query("SELECT * FROM trainings")
-    fun getTrainings(): LiveData<List<Training>>    //java compiling problem
+    fun getTrainings(): LiveData<List<Training>>
 
     @Query("SELECT * FROM trainings WHERE trainingId = (:trainingId)")
-    fun getTraining(trainingId: Int): LiveData<Training?>   //problem too
+    fun getTraining(trainingId: Int): LiveData<Training?>
 
     @Update
-    fun updateTraining(training: Training)    //same problem
+    fun updateTraining(training: Training)
 
     @Insert
-    fun addTraining(training: Training)    //and here
+    fun addTraining(training: Training)
 
     @Insert
-    fun deleteTraining(training: Training)    //here too
+    fun deleteTraining(training: Training)
 }
