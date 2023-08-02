8a5f1f2ccb0ed638d50c59dfa6c2c9adcf36ce00
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2023-08-02 23:58:56 +0300

    start screen updating problem, update unsolved

--- app/src/main/java/com/trainingtimer/data/TrainingDatabase.kt
+++ app/src/main/java/com/trainingtimer/data/TrainingDatabase.kt
@@ -41,7 +41,6 @@ abstract class TrainingDatabase : RoomDatabase() {
                         }*/
                     }
                 }
-                //update trainingList
             }
         })
             .build()
