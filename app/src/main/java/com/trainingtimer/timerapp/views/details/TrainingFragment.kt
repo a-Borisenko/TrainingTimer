ac8a39f7284a2b16b2c36c9efabe559e7c99cf08
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2023-09-17 11:33:32 +0300

    Keyboard bug fixed

--- app/src/main/java/com/trainingtimer/timerapp/views/details/TrainingFragment.kt
+++ app/src/main/java/com/trainingtimer/timerapp/views/details/TrainingFragment.kt
@@ -12,11 +12,8 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
@@ -37,7 +34,7 @@ class TrainingFragment : Fragment(R.layout.fragment_training) {
 //    private var timerState = TimerState.Stopped
     private var secondsRemaining = 0L
     private var trainingId = Training.UNDEFINED_ID
-    var trainNumber = 0
+    private var trainNumber = 0
 
     private lateinit var timer: CountDownTimer
     private lateinit var binding: FragmentTrainingBinding
@@ -167,29 +164,14 @@ class TrainingFragment : Fragment(R.layout.fragment_training) {
         )
     }
 
-    @SuppressLint("ResourceType")
-    private fun hideKeyboard() {
-        val view = childFragmentManager.findFragmentById(R.layout.fragment_training)
-        view.let {
-            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
-        }
-
-        val insets = ViewCompat.getRootWindowInsets(view ?: return)
-        val imeVisible = insets?.isVisible(WindowInsetsCompat.Type.ime())
-//        val imeHeight = insets?.getInsets(WindowInsetsCompat.Type.ime())?.bottom
-        if (imeVisible!!) {
-            val imm = getSystemService(requireContext(), this::class.java) as InputMethodManager
-            imm.hideSoftInputFromWindow(view?.windowToken, 0)
-        }
+    private fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
     }
 
-    fun hideKeyboard(activity: Activity) {
-        val view = activity.findViewById<View>(android.R.id.content)
-        if (view != null) {
-            val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
-        }
+    private fun Context.hideKeyboard(view: View) {
+        val inputMethodManager
+        = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun editTraining() {
