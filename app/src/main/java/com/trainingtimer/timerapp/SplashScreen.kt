8d141c917a433a3bb5630b2ba3b25d7e8c6cfca3
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2023-09-18 01:00:38 +0300

    App loading screen added

---
+++ app/src/main/java/com/trainingtimer/timerapp/SplashScreen.kt
@@ -0,0 +1,68 @@
+package com.trainingtimer.timerapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.trainingtimer.databinding.SplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+
+    private val splashDelay: Long = 1000 //3 seconds
+    private var mDelayHandler: Handler? = null
+    private var progressBarStatus = 0
+    private lateinit var binding: SplashScreenBinding
+    private var dummy: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, splashDelay)    //navigate with delay
+    }
+
+
+    private fun launchMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        this.finish()
+        mDelayHandler!!.removeCallbacks(mRunnable)
+
+    }
+
+    private val mRunnable: Runnable = Runnable {
+
+        Thread {
+            while (progressBarStatus < 100) {
+                //performing some dummy operation
+                try {
+                    dummy += 25
+                    Thread.sleep(100)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                //tracking progress
+                progressBarStatus = dummy
+
+                //updating the progress bar
+                binding.splashScreenProgressBar.progress = progressBarStatus
+            }
+            launchMainActivity()
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+}
