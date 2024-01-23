84295eb13edcbe56c38e1d9dac388c54705f3708
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-01-23 23:09:02 +0300

    Hilt problem; 0% bug & refactor left

--- app/src/main/java/com/trainingtimer/timerapp/App.kt
+++ app/src/main/java/com/trainingtimer/timerapp/App.kt
@@ -2,7 +2,9 @@ package com.trainingtimer.timerapp
 
 import android.app.Application
 import com.trainingtimer.timerapp.data.TrainingRepositoryImpl
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
