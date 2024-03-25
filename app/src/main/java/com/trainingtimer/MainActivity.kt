8367992c5636733ea07952866a74187372172390
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-03-25 23:35:05 +0300

    Launch from training when counting experiment; refactor left

--- app/src/main/java/com/trainingtimer/MainActivity.kt
+++ app/src/main/java/com/trainingtimer/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.trainingtimer
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.trainingtimer.views.details.TimerService
+import com.trainingtimer.views.details.TrainingFragment
 import com.trainingtimer.views.list.TrainingListFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,14 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (TimerService.isCounting) {
+            Log.d("main", "counting")
+            fragment = TrainingFragment()
+        } else {
+            Log.d("main", "stopped")
+            fragment = TrainingListFragment()
+        }
+
         val currentFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container)
         if (currentFragment == null) {
-            val fragment = TrainingListFragment()
+//            val fragment = TrainingListFragment()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
                 .commit()
         }
+    }
+
+    companion object {
+        private var fragment: Fragment = TrainingListFragment()
     }
 }
