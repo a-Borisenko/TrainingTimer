18ab394e3e07ad5c2aacb7870fb605a0262743d8
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-05-14 23:29:11 +0300

    DataService intent deleted; refactor left

--- app/src/main/java/com/trainingtimer/MainActivity.kt
+++ app/src/main/java/com/trainingtimer/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.trainingtimer
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
-import com.trainingtimer.utils.DataService
 import com.trainingtimer.views.details.TimerService
 import com.trainingtimer.views.details.TrainingFragment
 import com.trainingtimer.views.list.TrainingListFragment
@@ -16,8 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        startService(Intent(applicationContext, DataService::class.java))
 
         val currentFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container)
