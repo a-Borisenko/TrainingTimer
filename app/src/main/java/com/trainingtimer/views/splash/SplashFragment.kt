b7005aef20bcfa51266c0334fa840054e2c8d638
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-06-14 23:47:55 +0300

    SplashFragment simplifying; refactor left

--- app/src/main/java/com/trainingtimer/views/splash/SplashFragment.kt
+++ app/src/main/java/com/trainingtimer/views/splash/SplashFragment.kt
@@ -2,22 +2,19 @@ package com.trainingtimer.views.splash
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.trainingtimer.R
 import com.trainingtimer.databinding.SplashScreenBinding
-import com.trainingtimer.utils.LoadingState
-import com.trainingtimer.utils.hide
-import com.trainingtimer.utils.show
 import com.trainingtimer.views.list.TrainingListFragment
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment(R.layout.splash_screen) {
 
-    private val viewModel: SplashViewModel by viewModels()
+//    private val viewModel: SplashViewModel by viewModels()
     private lateinit var binding: SplashScreenBinding
+    private var progressBarStatus = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,7 +22,13 @@ class SplashFragment : Fragment(R.layout.splash_screen) {
 
         // Наблюдайте за состоянием загрузки данных
         lifecycleScope.launch {
-            viewModel.loadingStateFlow.collect { state ->
+            while (progressBarStatus < 100) {
+                progressBarStatus += 25
+                delay(100)
+                binding.splashScreenProgressBar.progress = progressBarStatus
+            }
+            navigateToMainScreen()
+            /*viewModel.loadingStateFlow.collect { state ->
                 when (state) {
                     is LoadingState.Loading -> {
                         // Показать индикатор загрузки
@@ -42,25 +45,27 @@ class SplashFragment : Fragment(R.layout.splash_screen) {
                         showErrorMessage(state.error)
                     }
                 }
-            }
+            }*/
         }
 
         // Если возвращаемся на этот экран, обновить данные
-        lifecycleScope.launch {
+        /*lifecycleScope.launch {
             viewModel.updateData()
-        }
+        }*/
     }
 
     private fun navigateToMainScreen() {
         // Перейти к основному экрану приложения
+        requireActivity().supportFragmentManager
+            .popBackStack()
         requireActivity().supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_container, TrainingListFragment())
             .commit()
     }
 
-    private fun showErrorMessage(error: Throwable) {
+    /*private fun showErrorMessage(error: Throwable) {
         // Показать сообщение об ошибке пользователю
         Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
-    }
+    }*/
 }
