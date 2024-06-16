fd9e106f723f198081f233de315ef0b9f11cdbe9
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-06-16 23:51:46 +0300

    New SplashFragment + viewModel; refactor left

--- app/src/main/java/com/trainingtimer/views/splash/SplashFragment.kt
+++ app/src/main/java/com/trainingtimer/views/splash/SplashFragment.kt
@@ -1,20 +1,21 @@
 package com.trainingtimer.views.splash
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.trainingtimer.MainActivity
 import com.trainingtimer.R
 import com.trainingtimer.databinding.SplashScreenBinding
-import com.trainingtimer.views.list.TrainingListFragment
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment(R.layout.splash_screen) {
 
-//    private val viewModel: SplashViewModel by viewModels()
+    private val viewModel: SplashViewModel by viewModels()
     private lateinit var binding: SplashScreenBinding
-    private var progressBarStatus = 0
+//    private var progressBarStatus = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,12 +23,13 @@ class SplashFragment : Fragment(R.layout.splash_screen) {
 
         // Наблюдайте за состоянием загрузки данных
         lifecycleScope.launch {
-            while (progressBarStatus < 100) {
-                progressBarStatus += 25
-                delay(100)
-                binding.splashScreenProgressBar.progress = progressBarStatus
+            viewModel.progressFlow.collect { progress ->
+                binding.splashScreenProgressBar.progress = progress
+                if (progress == 100) {
+                    launchMainActivity()
+                }
             }
-            navigateToMainScreen()
+//            navigateToMainScreen()
             /*viewModel.loadingStateFlow.collect { state ->
                 when (state) {
                     is LoadingState.Loading -> {
@@ -48,13 +50,24 @@ class SplashFragment : Fragment(R.layout.splash_screen) {
             }*/
         }
 
+        lifecycleScope.launch {
+            viewModel.startSplashTimer()
+        }
+
         // Если возвращаемся на этот экран, обновить данные
         /*lifecycleScope.launch {
             viewModel.updateData()
         }*/
     }
 
-    private fun navigateToMainScreen() {
+    private fun launchMainActivity() {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        requireActivity().finish()
+    }
+
+    /*private fun navigateToMainScreen() {
         // Перейти к основному экрану приложения
         requireActivity().supportFragmentManager
             .popBackStack()
@@ -62,7 +75,7 @@ class SplashFragment : Fragment(R.layout.splash_screen) {
             .beginTransaction()
             .replace(R.id.fragment_container, TrainingListFragment())
             .commit()
-    }
+    }*/
 
     /*private fun showErrorMessage(error: Throwable) {
         // Показать сообщение об ошибке пользователю
