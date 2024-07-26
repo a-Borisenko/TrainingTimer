463251a95cf882e81e6c0a6eded03a882a7b9774
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-07-27 02:32:27 +0300

    CalendarFragment refactoring; launch from Notification & save 0 sec crush; refactor left

--- app/src/main/java/com/trainingtimer/views/calendar/CalendarFragment.kt
+++ app/src/main/java/com/trainingtimer/views/calendar/CalendarFragment.kt
@@ -7,12 +7,17 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.trainingtimer.R
 import com.trainingtimer.databinding.FragmentCalendarBinding
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 import java.util.Date
+import java.util.Locale
 
 class CalendarFragment : Fragment(R.layout.fragment_calendar) {
 
@@ -42,17 +47,96 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar) {
 //        setupSwipeListener(rvDayList)
 
 
-        /*val today = Calendar.getInstance()
+//        val today = Calendar.getInstance()
 
-        binding.calendarRecyclerView.date = today.timeInMillis
+//        binding.calendarRecyclerView.date = today.timeInMillis
 
-        binding.calendarRecyclerView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+        /*binding.calendarRecyclerView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             // Обработка выбранной даты
             val selectedDate = Calendar.getInstance()
             selectedDate.set(year, month, dayOfMonth)
             Toast.makeText(context, "date $dayOfMonth.$month.$year", Toast.LENGTH_SHORT).show()
             // Выполните необходимые действия с выбранной датой
         }*/
+
+        val calendar = Calendar.getInstance()
+
+        // Start from the current month
+        selectedMonthDate = calendar.time
+        binding.monthYearTv.text = dateFormatter(selectedMonthDate!!)
+
+        // Initiate the months list
+//        startList()
+
+        // Start from the center of the list
+        latestPos = loadedDates.size/2
+
+        // Create dummy events list
+        val events = mutableListOf<Date>()
+        for(i in 1..10){
+            calendar.add(java.util.Calendar.DATE,i)
+            events.add(calendar.time)
+        }
+
+        /*val adapter = PageAdapter(requireContext(),events){
+            val sdf = SimpleDateFormat("EE dd/MM/yyyy", Locale.getDefault())
+            Toast.makeText(requireContext(),"Selected date is : ${sdf.format(it)}",Toast.LENGTH_LONG).show()
+        }*/
+//        binding.pageRecyclerView.adapter = adapter
+        binding.calendarRecyclerView.itemAnimator = null
+        listAdapter.submitList(loadedDates)
+        val layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL,false)
+        binding.pageRecyclerView.layoutManager = layoutManager
+
+        // Add PagerSnapHelper to make RecyclerView scroll like a Pager
+        val snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(binding.pageRecyclerView)
+
+        // Start calendar from the current month
+        binding.pageRecyclerView.scrollToPosition(listAdapter.getItemPos(selectedMonthDate!!))
+
+        binding.pageRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if(newState == RecyclerView.SCROLL_STATE_IDLE){
+                    // Get the position of the selected month
+                    val pos = layoutManager.findFirstVisibleItemPosition()
+
+                    // If switched to a different month
+                    if(latestPos != pos){
+                        // Assign new selected date
+                        selectedMonthDate = adapter.getItemByPos(pos)
+                        binding.monthText.text = dateFormatter(selectedMonthDate!!)
+                        // Assign new latest position
+                        latestPos = pos
+
+                        // If at the start of the list, load previous months and scroll back to the same month
+                        if(pos == 0){
+                            loadPreviousMonths()
+                            adapter.submitList(loadedDates.toMutableList())
+                            recyclerView.scrollToPosition(adapter.getItemPos(selectedMonthDate!!))
+                        }
+                        // If at the end of the list, load next months and scroll back to the same month
+                        if(pos == loadedDates.size-1){
+                            loadNextMonths()
+                            adapter.submitList(loadedDates.toMutableList())
+                            recyclerView.scrollToPosition(adapter.getItemPos(selectedMonthDate!!))
+                        }
+                    }
+                }
+            }
+        })
+        binding.nextMonth.setOnClickListener{
+            // Go to the next month
+            if(latestPos +1 <= loadedDates.size-1)
+                binding.pageRecyclerView.smoothScrollToPosition(latestPos+1)
+        }
+        binding.previousMonth.setOnClickListener {
+            // Go to the previous month
+            if(latestPos - 1 >= 0){
+                binding.pageRecyclerView.smoothScrollToPosition(latestPos-1)
+            }
+        }
     }
 
     private fun setupClickListener() {
