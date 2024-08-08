6cc7aa961ff8355431b0d4d674cea256f44b2092
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-08-08 23:33:24 +0300

    New MonthViewHolder

---
+++ app/src/main/java/com/trainingtimer/views/calendar/month/MonthViewHolder.kt
@@ -0,0 +1,9 @@
+package com.trainingtimer.views.calendar.month
+
+import androidx.recyclerview.widget.RecyclerView
+import com.trainingtimer.databinding.PageRecyclerItemBinding
+
+class MonthViewHolder(val binding: PageRecyclerItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    val daysRecycler = binding.daysRecycler
+}
