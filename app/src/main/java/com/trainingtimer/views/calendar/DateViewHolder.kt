1f313183bb25cb1c02ad2e6c57d10a00ff13f37d
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-08-13 00:51:44 +0300

    New DateViewHolder

---
+++ app/src/main/java/com/trainingtimer/views/calendar/DateViewHolder.kt
@@ -0,0 +1,10 @@
+package com.trainingtimer.views.calendar
+
+import androidx.recyclerview.widget.RecyclerView
+import com.trainingtimer.databinding.CalendarCellBinding
+
+class DateViewHolder(val binding: CalendarCellBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    val textView = binding.textView
+    val backgroundConstraint = binding.backgroundConstraint
+}
