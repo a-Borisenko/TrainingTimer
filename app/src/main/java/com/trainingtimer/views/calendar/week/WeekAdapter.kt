a30cf930a9ffdc615fa0f04e4fea6cdfb028b37a
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-10-20 22:56:29 +0300

    New Week data class

--- app/src/main/java/com/trainingtimer/views/calendar/week/WeekAdapter.kt
+++ app/src/main/java/com/trainingtimer/views/calendar/week/WeekAdapter.kt
@@ -1,13 +1,11 @@
 package com.trainingtimer.views.calendar.week
 
-import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.trainingtimer.databinding.CalendarCellBinding
-import com.trainingtimer.domain.CalendarDay
 
-class WeekAdapter : ListAdapter<CalendarDay, WeekViewHolder>(WeekDiffCallBack()) {
+class WeekAdapter : ListAdapter<Week, WeekViewHolder>(WeekDiffCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeekViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -16,13 +14,6 @@ class WeekAdapter : ListAdapter<CalendarDay, WeekViewHolder>(WeekDiffCallBack())
     }
 
     override fun onBindViewHolder(holder: WeekViewHolder, position: Int) {
-        // Рассчитываем высоту элемента
-        val screenHeight = Resources.getSystem().displayMetrics.heightPixels
-        val weekCount = 6  // Максимум 6 недель в месяце
-        val itemHeight = screenHeight / weekCount  // Равномерное распределение высоты на строки
-
-        val layoutParams = holder.itemView.layoutParams
-        layoutParams.height = itemHeight
-        holder.itemView.layoutParams = layoutParams
+        holder.bind(getItem(position))
     }
 }
