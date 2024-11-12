69ddc19bded418664d08db2fe4588f5e21167fc4
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-11-13 00:14:54 +0300

    New week RecyclerView

---
+++ app/src/main/java/com/trainingtimer/views/calendar/month/WeekDiffCallback.kt
@@ -0,0 +1,15 @@
+package com.trainingtimer.views.calendar.month
+
+import androidx.recyclerview.widget.DiffUtil
+import com.trainingtimer.domain.CalendarDay
+
+class WeekDiffCallback : DiffUtil.ItemCallback<List<CalendarDay>>() {
+
+    override fun areItemsTheSame(oldItem: List<CalendarDay>, newItem: List<CalendarDay>): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: List<CalendarDay>, newItem: List<CalendarDay>): Boolean {
+        return oldItem == newItem
+    }
+}
---
+++ app/src/main/java/com/trainingtimer/views/calendar/month/WeekViewHolder.kt
@@ -0,0 +1,19 @@
+package com.trainingtimer.views.calendar.month
+
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.trainingtimer.databinding.PageRecyclerItemBinding
+import com.trainingtimer.views.calendar.date.DateAdapter
+
+class WeekViewHolder(val binding: PageRecyclerItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    private val daysRecycler = binding.daysRecycler
+
+    fun bindHolder(adapter: DateAdapter) {
+        if (daysRecycler.layoutManager == null) {
+            daysRecycler.layoutManager = GridLayoutManager(binding.root.context, 7)
+            daysRecycler.itemAnimator = null
+        }
+        daysRecycler.adapter = adapter
+    }
+}
