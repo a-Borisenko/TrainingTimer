31839fe009f0cd91e5bed8b1200567868cdf56d8
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-11-13 00:16:49 +0300

    New week RecyclerView; week queue problem

--- app/src/main/java/com/trainingtimer/views/calendar/month/WeekAdapter.kt
+++ app/src/main/java/com/trainingtimer/views/calendar/month/WeekAdapter.kt
@@ -3,53 +3,44 @@ package com.trainingtimer.views.calendar.month
 import android.content.Context
 import android.content.res.Resources
 import android.os.Build
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowInsets
 import androidx.core.view.doOnAttach
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import com.trainingtimer.databinding.PageRecyclerItemBinding
+import com.trainingtimer.domain.CalendarDay
 import com.trainingtimer.views.calendar.date.DateAdapter
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
-class MonthAdapter(
+class WeekAdapter(
     val context: Context,
     private val events: List<Date>,
     val onItemClick: (date: Date?) -> Unit
-) : ListAdapter<Date, MonthViewHolder>(MonthDiffCallBack()) {
+) : ListAdapter<List<CalendarDay>, WeekViewHolder>(WeekDiffCallback()) {
 
     private var selectedInfo: Pair<Date?, Int?> = null to null
-    private val monthCalculator = MonthCalculator()
     private val dateAdapterCache = mutableMapOf<Int, DateAdapter>()
     private var itemHeight = 0
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonthViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeekViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = PageRecyclerItemBinding.inflate(inflater, parent, false)
-        return MonthViewHolder(binding)
+        return WeekViewHolder(binding)
     }
 
-    fun getItemPos(date: Date): Int {
-        return currentList.indexOfFirst { areDatesEqual(it, date) }
-    }
-
-    fun getItemByPos(pos: Int): Date {
-        return currentList[pos]
-    }
-
-    override fun onBindViewHolder(holder: MonthViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: WeekViewHolder, position: Int) {
         val screenHeight = Resources.getSystem().displayMetrics.heightPixels
-        val weekCount = 6
 
         holder.binding.root.apply {
             doOnAttach {
                 itemHeight = if (isGesture(it)) {
-                    screenHeight / (weekCount + 1)
+                    screenHeight / 7
                 } else {
-                    screenHeight / (weekCount + 2)
+                    screenHeight / 8
                 }
             }
         }
@@ -58,11 +49,7 @@ class MonthAdapter(
         layoutParams.height = itemHeight
         holder.itemView.layoutParams = layoutParams
 
-        val date = getItem(position)
-//        val list = monthCalculator.getDaysInMonth(date)
-        val previousList = monthCalculator.getPreviousWeek(date)
-        val thisWeek = monthCalculator.getThisWeek(date)
-        val nextWeeks = monthCalculator.getNextWeeks(date)
+        val week = getItem(position)
 
         val adapter = dateAdapterCache.getOrPut(position) {
             DateAdapter(events, context, selectedInfo.first) { calendarDay ->
@@ -70,27 +57,7 @@ class MonthAdapter(
             }
         }
         holder.bindHolder(adapter)
-        adapter.submitList(previousList + thisWeek + nextWeeks)
-    }
-
-    private fun isGesture(view: View): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                (view.rootWindowInsets?.getInsets(WindowInsets.Type.systemGestures())?.left
-                    ?: 0) > 0
-            } else {
-                TODO("VERSION.SDK_INT = Q")
-            }
-        } else false
-    }
-
-    private fun MonthViewHolder.bindHolder(adapter: DateAdapter) {
-        if (daysRecycler.layoutManager == null) {
-            daysRecycler.layoutManager = GridLayoutManager(context, 7)
-            daysRecycler.itemAnimator = null
-            Log.d("MonthAdapter", "layoutManager = GridLayoutManager(context, 7)")
-        }
-        daysRecycler.adapter = adapter
+        adapter.submitList(week)
     }
 
     private fun onDateSelected(selectedDate: Date?, position: Int) {
@@ -103,5 +70,33 @@ class MonthAdapter(
             notifyItemChanged(position)
             onItemClick(selectedInfo.first)
         }
+    }
+
+    private fun areDatesEqual(dateFirst: Date?, dateSecond: Date?): Boolean {
+        val sdf = SimpleDateFormat("yyyyMMdd", Locale.getDefault())
+        if (dateFirst == null || dateSecond == null) return false
+        return sdf.format(dateFirst) == sdf.format(dateSecond)
+    }
+
+    fun getItemPos(selectedDate: Date): Int {
+        for (index in currentList.indices) {
+            val week = currentList[index]
+            // Проверяем, содержит ли неделя (список `CalendarDay`) выбранную дату
+            if (week.any { areDatesEqual(it.date, selectedDate) }) {
+                return index
+            }
+        }
+        return -1 // Возвращаем -1, если дата не найдена
+    }
+
+    private fun isGesture(view: View): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                (view.rootWindowInsets?.getInsets(WindowInsets.Type.systemGestures())?.left
+                    ?: 0) > 0
+            } else {
+                TODO("VERSION.SDK_INT = Q")
+            }
+        } else false
     }
 }
