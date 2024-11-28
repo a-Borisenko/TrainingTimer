9d44e90c29d72e26fb9d1a43224fd8f36db019ea
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-11-28 23:43:51 +0300

    Extra commits deleted

--- app/src/main/java/com/trainingtimer/views/calendar/date/DateViewHolder.kt
+++ app/src/main/java/com/trainingtimer/views/calendar/date/DateViewHolder.kt
@@ -20,21 +20,14 @@ class DateViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(date: CalendarDay) {
-        // Всегда устанавливаем клик
         binding.root.setOnClickListener {
             onItemClick(date)
         }
 
-        // Установка текста дня
         binding.textView.text = date.dayOfMonth
-
-        // Проверка: дата совпадает с текущей?
         val isToday = areDatesEqual(Calendar.getInstance().time, date.date)
-
-        // Проверка: день содержит событие?
         val isEventDay = events.any { areDatesEqual(it, date.date) }
 
-        // Настройка стиля в зависимости от условий
         when {
             isToday -> binding.backgroundConstraint.background =
                 getDrawable(context, R.drawable.calendar_cell_today)
@@ -46,7 +39,6 @@ class DateViewHolder(
                 getDrawable(context, R.drawable.calendar_cell_background)
         }
 
-        // Оформление для дат вне текущего месяца
         if (!isInTheSelectedMonth(date.date)) {
             binding.textView.setTextColor(Color.parseColor("#D3D3D3"))
         } else {
