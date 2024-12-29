934d0035a29fc6314a66e0702a3094e53e400911
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2024-12-30 00:13:31 +0300

    onClick logic moved to TrainingViewHolder

--- app/src/main/java/com/trainingtimer/views/list/TrainingAdapter.kt
+++ app/src/main/java/com/trainingtimer/views/list/TrainingAdapter.kt
@@ -3,41 +3,24 @@ package com.trainingtimer.views.list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.trainingtimer.databinding.ListItemTrainingBinding
 import com.trainingtimer.domain.Training
 
-class TrainingAdapter : ListAdapter<Training, TrainingViewHolder>(TrainingDiffCallback()) {
-
-    var onTrainingLongClickListener: ((Training) -> Unit)? = null
-    var onTrainingClickListener: ((Training) -> Unit)? = null
+class TrainingAdapter(
+    private val onItemClick: (Training) -> Unit,
+    private val onItemLongClick: (Training) -> Unit
+) : ListAdapter<Training, TrainingViewHolder>(TrainingDiffCallback()) {
 
 //    val MAX_POOL_SIZE = 10
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrainingViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemTrainingBinding.inflate(inflater, parent, false)
-        return TrainingViewHolder(binding).apply {
-            binding.root.setOnClickListener {
-                val position = bindingAdapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    onTrainingClickListener?.invoke(getItem(position))
-                }
-            }
-            binding.root.setOnLongClickListener {
-                val position = bindingAdapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    onTrainingLongClickListener?.invoke(getItem(position))
-                    true
-                } else {
-                    false
-                }
-            }
-        }
+        return TrainingViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TrainingViewHolder, position: Int) {
         val training = getItem(position)
-        holder.bind(training)
+        holder.bind(training, onItemClick, onItemLongClick)
     }
 }
