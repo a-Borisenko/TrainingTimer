a465de88ce4c571443b0df6533c027b4c9ba92ca
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2025-01-03 22:00:03 +0300

    Redundant code deleted

--- app/src/main/java/com/trainingtimer/views/list/TrainingAdapter.kt
+++ app/src/main/java/com/trainingtimer/views/list/TrainingAdapter.kt
@@ -11,8 +11,6 @@ class TrainingAdapter(
     private val onItemLongClick: (Training) -> Unit
 ) : ListAdapter<Training, TrainingViewHolder>(TrainingDiffCallback()) {
 
-//    val MAX_POOL_SIZE = 10
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrainingViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemTrainingBinding.inflate(inflater, parent, false)
