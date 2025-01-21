0b22f86e99fff5e5de869c66c0d32cfc201e640f
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2025-01-21 23:18:52 +0300

    simple collect instead of extension

--- app/src/main/java/com/trainingtimer/views/details/TrainingFragment.kt
+++ app/src/main/java/com/trainingtimer/views/details/TrainingFragment.kt
@@ -10,11 +10,11 @@ import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.trainingtimer.R
 import com.trainingtimer.databinding.FragmentTrainingBinding
 import com.trainingtimer.utils.DataService.Companion.START
-import com.trainingtimer.utils.collectInViewScope
 import com.trainingtimer.utils.onChange
 import com.trainingtimer.utils.timeLongToString
 import com.trainingtimer.utils.timeStringToLong
@@ -43,22 +43,24 @@ class TrainingFragment : Fragment(R.layout.fragment_training) {
     }
 
     private fun observeViewModel() {
-        viewModel.state.collectInViewScope(this) { state ->
-            binding.etSets.setText(state.sets)
-            binding.etTitle.setText(state.title)
-            binding.etTimes.setText(state.times)
-            binding.viewTimer.text = timeLongToString(state.secRemain)
-            binding.countdownBar.progress = state.progress.toInt()
+        lifecycleScope.launchWhenStarted {
+            viewModel.state.collect { state ->
+                binding.etSets.setText(state.sets)
+                binding.etTitle.setText(state.title)
+                binding.etTimes.setText(state.times)
+                binding.viewTimer.text = timeLongToString(state.secRemain)
+                binding.countdownBar.progress = state.progress.toInt()
 
-            binding.tilSets.error =
-                if (state.errorInputSets) getString(R.string.error_input_sets) else null
-            binding.tilTitle.error =
-                if (state.errorInputTitle) getString(R.string.error_input_title) else null
-            binding.tilTimes.error =
-                if (state.errorInputTimes) getString(R.string.error_input_times) else null
+                binding.tilSets.error =
+                    if (state.errorInputSets) getString(R.string.error_input_sets) else null
+                binding.tilTitle.error =
+                    if (state.errorInputTitle) getString(R.string.error_input_title) else null
+                binding.tilTimes.error =
+                    if (state.errorInputTimes) getString(R.string.error_input_times) else null
 
-            if (state.shouldCloseScreen) {
-                findNavController().popBackStack()
+                if (state.shouldCloseScreen) {
+                    findNavController().popBackStack()
+                }
             }
         }
     }
