c0c4aca55ecc84edd7f3cc2851eb9f48518d0839
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2025-02-15 23:50:14 +0300

    Redundant code deleted

--- app/src/main/java/com/trainingtimer/presentation/calendar/CalendarState.kt
+++
@@ -1,9 +0,0 @@
-package com.trainingtimer.presentation.calendar
-
-import com.trainingtimer.domain.entity.Day
-import java.util.Date
-
-sealed class CalendarState(
-    val selectedWeekDate: Date,
-    val loadedWeeks: List<List<Day>>
-)
