2eb2fbe20eb280db4a5c649b7feaa0729e1091e4
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2025-05-06 23:13:08 +0300

    Shortening errors names

--- app/src/main/java/com/trainingtimer/presentation/details/TrainingState.kt
+++ app/src/main/java/com/trainingtimer/presentation/details/TrainingState.kt
@@ -6,9 +6,9 @@ data class TrainingState(
     val times: String = "",
     val secRemain: String = "00:00",
     val progress: Int = 0,
-    val errorInputSets: Boolean = false,
-    val errorInputTitle: Boolean = false,
-    val errorInputTimes: Boolean = false,
+    val errorSets: Boolean = false,
+    val errorTitle: Boolean = false,
+    val errorTimes: Boolean = false,
     val isCounting: Boolean = false,
     val shouldCloseScreen: Boolean = false
 )
